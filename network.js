198c093acb5bc46cbb71cffc4b5b156aad7d57bb
dnlbui <dnlbui@proton.me>
2025-02-28 18:40:30 -0600

    Update network configuration for local development

--- network.js
+++ network.js
@@ -1,11 +1,11 @@
 const network = {
-  netid: "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
+  "netid": "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
   "name": "Testnet",
-  gateways: [
+  "gateways": [
     {
-      protocol: "https",
-      host: "test.liberdus.com",
-      port: 3030,
+      "protocol": "http",
+      "host": "localhost",
+      "port": 3030    
     },
   ],
   "monitor": {
