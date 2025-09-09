bc66345445544ce3128fc9909fc57acda4ef6431
Daniel Bui <62664081+dnlbui@users.noreply.github.com>
2025-09-09 11:10:31 -0500

    Feat: no call notification on phone when a scheduled call message is sent (#788)
    * Update callType assignment logic in chat message creation to conditionally set based on callObj.callTime
    
    * Update comment in chat message creation to clarify callType logic for recipient phone ringing

--- app.js
+++ app.js
@@ -9791,7 +9791,10 @@ console.warn('in send message', txid)
       // Create and send the call message transaction
       let tollInLib = myData.contacts[currentAddress].tollRequiredToSend == 0 ? 0n : this.toll;
       const chatMessageObj = await this.createChatMessage(currentAddress, payload, tollInLib, keys);
-      chatMessageObj.callType = true
+      // if there's a callobj.calltime is present and is 0 set callType to true to make recipient phone ring
+      if (callObj?.callTime === 0) {
+        chatMessageObj.callType = true;
+      }
 
       await signObj(chatMessageObj, keys);
       const txid = getTxid(chatMessageObj);
