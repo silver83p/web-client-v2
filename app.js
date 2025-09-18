7687e0ce979312fd86dbd33a1e5c942789449f83
silver83p <silver83p@gmail.com>
2025-09-18 16:25:36 -0500

    Refactor username handling in processChats function to streamline logic. (#876)
    * Refactor username handling in processChats function to streamline logic.
    
    * Refactor username validation logic in processChats function fixing some indentation issues.

--- app.js
+++ app.js
@@ -3860,24 +3860,22 @@ async function processChats(chats, keys) {
           if (payload.senderInfo && !mine){
             contact.senderInfo = cleanSenderInfo(payload.senderInfo)
             delete payload.senderInfo;
-            if (!contact.username && contact.senderInfo.username) {
+            if (contact.username) {
+              // if we already have the username, we can use it
+              contact.senderInfo.username = contact.username;
+            } else if (contact.senderInfo.username) {
               // check if the username given with the message maps to the address of this contact
               const usernameAddress = await getUsernameAddress(contact.senderInfo.username);
-                if (usernameAddress && normalizeAddress(usernameAddress) === normalizeAddress(tx.from)) {
-                  contact.username = contact.senderInfo.username;
-                } else {
-                  // username doesn't match address so skipping this message
-                  console.error(`Username: ${contact.senderInfo.username} does not match address ${tx.from}`);
-                  continue;
-                }
-            } else {
-              if(contact.username) {
-                // if we already have the username, we can use it
-                contact.senderInfo.username = contact.username;
+              if (usernameAddress && normalizeAddress(usernameAddress) === normalizeAddress(tx.from)) {
+                contact.username = contact.senderInfo.username;
               } else {
-                console.error(`Username not provided in senderInfo.`)
-                continue
+                // username doesn't match address so skipping this message
+                console.error(`Username: ${contact.senderInfo.username} does not match address ${tx.from}`);
+                continue;
               }
+            } else {
+              console.error(`Username not provided in senderInfo.`)
+              continue
             }
           }
           //  skip if this tx was processed before and is already in contact.messages;
@@ -3977,24 +3975,22 @@ async function processChats(chats, keys) {
           if (payload.senderInfo && !mine) {
             contact.senderInfo = cleanSenderInfo(payload.senderInfo);
             delete payload.senderInfo;
-            if (!contact.username && contact.senderInfo.username) {
+            if (contact.username) {
+              // if we already have the username, we can use it
+              contact.senderInfo.username = contact.username;
+            } else if (contact.senderInfo.username) {
               // check if the username given with the message maps to the address of this contact
               const usernameAddress = await getUsernameAddress(contact.senderInfo.username);
-                if (usernameAddress && normalizeAddress(usernameAddress) === normalizeAddress(tx.from)) {
-                  contact.username = contact.senderInfo.username;
-                } else {
-                  // username doesn't match address so skipping this message
-                  console.error(`Username: ${contact.senderInfo.username} does not match address ${tx.from}`);
-                  continue;
-                }
-            } else {
-              if(contact.username) {
-                // if we already have the username, we can use it
-                contact.senderInfo.username = contact.username;
+              if (usernameAddress && normalizeAddress(usernameAddress) === normalizeAddress(tx.from)) {
+                contact.username = contact.senderInfo.username;
               } else {
-                console.error(`Username not provided in senderInfo.`)
-                continue
+                // username doesn't match address so skipping this message
+                console.error(`Username: ${contact.senderInfo.username} does not match address ${tx.from}`);
+                continue;
               }
+            } else {
+              console.error(`Username not provided in senderInfo.`)
+              continue
             }
           }
 
